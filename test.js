cdeb71bbdf01d8e812d14143e717cebebb216763
Justin Maat <justin.maat@wendys.com>
2015-10-13 10:25:11 -0400

    adding tests

--- test.js
+++ test.js
@@ -3,27 +3,29 @@ const test = require('tape-catch');
 const allOtherThingsBeingEqual = require('./');
 
 test('all methods available from root', assert => {
-  assert.plan(4);
-  [
+    assert.plan(5);
+[
     'networkIsReliable',
     'networkIsSecure',
     'latencyIsZero',
-    'bandwidthIsInfinite'
-  ].forEach(m => assert.notOk(allOtherThingsBeingEqual[m](), m));
+    'bandwidthIsInfinite',
+    'codeCoverageEqualsTestCaseCoverage'
+].forEach(m => assert.notOk(allOtherThingsBeingEqual[m](), m));
 });
 
 test('all methods available separately', assert => {
-  assert.plan(4);
-  [
+    assert.plan(5);
+[
     'network-is-reliable',
     'network-is-secure',
     'latency-is-zero',
-    'bandwidth-is-infinite'
-  ].forEach(m => assert.notOk(require('./' + m)(), m));
+    'bandwidth-is-infinite',
+    'code-coverage-equals-test-case-coverage'
+].forEach(m => assert.notOk(require('./' + m)(), m));
 });
 
 test('all other things being equal', assert => {
-  assert.plan(1);
-  assert.notOk(allOtherThingsBeingEqual(), 'nope');
+    assert.plan(1);
+assert.notOk(allOtherThingsBeingEqual(), 'nope');
 });
 
