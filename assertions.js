fab52a2e6017c6707afdac37d5cab609863844a4
Justin Maat <justin.maat@wendys.com>
2015-10-13 10:26:33 -0400

    adding new important functionality for checking if code coverage equals the test case coverage

--- assertions.js
+++ assertions.js
@@ -1,23 +1,35 @@
+var bandwidthIsInfinit = require('./bandwidth-is-infinite');
+var networkIsReliable = require('./network-is-reliable');
+var networkIsSecure = require('./network-is-secure');
+var latencyIsZero = require('./latency-is-zero');
+var bandwidthIsInfinite = require('./bandwidth-is-infinite');
+var codeCoverageEqualsTestCaseCoverage = require('./code-coverage-equals-test-case-coverage');
+
+
 function allOtherThingsBeingEqual() {
-  return false;
+    return false;
 }
 
 allOtherThingsBeingEqual.networkIsReliable = function() {
-  return false;
+    return false;
 }
 
 allOtherThingsBeingEqual.networkIsSecure = function() {
-  return false;
+    return false;
 }
 
 allOtherThingsBeingEqual.latencyIsZero = function() {
-  return false;
+    return false;
 }
 
 allOtherThingsBeingEqual.bandwidthIsInfinite = function() {
-  return false;
+    return false;
+}
+
+allOtherThingsBeingEqual.codeCoverageEqualsTestCaseCoverage = function () {
+    return false;
 }
 
 if (typeof module !== "undefined") {
-  module.exports = allOtherThingsBeingEqual;
+    module.exports = allOtherThingsBeingEqual;
 }
